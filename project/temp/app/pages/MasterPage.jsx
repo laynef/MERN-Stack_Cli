cffdbee2af38cfcf18aecdb1eab6203027a8fb96
laynefaler <laynefaler@gmail.com>
2017-01-11 12:09:50 -0800

    Add React Bootstrap

--- project/temp/app/pages/MasterPage.jsx
+++ project/temp/app/pages/MasterPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Router, Route, Link, browserHistory } from 'react-router'
+import { NavbarHeader, NavbarToggle, NavbarCollapse, NavbarBrand, Navbar, Nav, NavItem, MenuItem, NavDropdown, Button } from 'react-bootstrap'
 
 export default class MasterPage extends React.Component {
 
@@ -9,6 +11,20 @@ export default class MasterPage extends React.Component {
     render() {
         return (
             <div>
+                <Navbar fixedTop inverse collapseOnSelect>
+                    <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">MERN Stack Cli</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem eventKey={1}><Link className="topNavLink" to="/">Link</Link></NavItem>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                
                 {this.props.children}
             </div>
             )
